f30f9751939bfe3c139bea2fd21aca2103b262cb
C L <c1000@protonmail.com>
2024-02-14 16:17:46 -0300

    colors moved to a separate file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
+import tailwindColors from './tailwind.colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      colors: {
-        beige: {
-          50: "#FBF8F4",
-          100: "#F8F4EC",
-          200: "#F0E6D6",
-          300: "#E9DBC2",
-          400: "#D9C196",
-          500: "#C9A669",
-          600: "#B48A41",
-          700: "#876731",
-          800: "#5A4520",
-          900: "#2D2210",
-          950: "#161108"
-        },
+      colors: tailwindColors,
+      fontFamily: {
+        // "sans": ["Nunito", "sans-serif"],
+        // "sidebar": ["Quicksand", "sans-serif"],
+        // "mono": ["IBM Plex Mono", "monospace"],
       },
     },
   },
