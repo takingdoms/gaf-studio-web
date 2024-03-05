3bae08f9ec993ceb58a871d76f2e27811447d0da
C L <c1000@protonmail.com>
2024-03-05 18:38:36 -0300

    multi-layered frames now show the first sub-layer on the selector panel

--- src/components/app/frame-selector/FrameSelectorItem.tsx
+++ src/components/app/frame-selector/FrameSelectorItem.tsx
@@ -10,6 +10,11 @@ type FrameSelectorItemProps = {
   onClick: () => void;
 };
 
+// TODO put some constant/config somewhere that tells whether the first subframe to be selected
+// by default on frame change is null or the first subframe available. If it's null then the
+// value below show be set to true otherwise to false
+const COMPOSITE_MULTI_LAYERS: boolean = false;
+
 export default function FrameSelectorItem({
   index,
   frameData,
@@ -19,6 +24,40 @@ export default function FrameSelectorItem({
   const borderCls = isSelected ? 'border-blue-500' : 'border-gray-300';
   const textCls = isSelected ? 'text-blue-500' : 'text-gray-400';
   const cursorCls = isSelected ? 'cursor-default' : 'cursor-pointer';
+
+  let image: React.ReactNode;
+
+  if (frameData.kind === 'single') {
+    image = (
+      <FrameDataImage
+        frameData={frameData}
+        contain={true}
+        smoothing={false}
+      />
+    );
+  }
+  else {
+    if (COMPOSITE_MULTI_LAYERS || frameData.layers.length === 0) {
+      image = (
+        <FrameDataCompositeImage
+          frameData={frameData}
+          contain={true}
+          smoothing={false}
+        />
+      );
+    }
+    else {
+      const firstLayer = frameData.layers[0];
+
+      image = (
+        <FrameDataImage
+          frameData={firstLayer}
+          contain={true}
+          smoothing={false}
+        />
+      );
+    }
+  }
 
   return (
     <div
@@ -34,21 +73,7 @@ export default function FrameSelectorItem({
           <span>#{index + 1}</span>
         </div>
         <div className="basis-0 grow overflow-hidden flex justify-center items-center">
-          {frameData.kind === 'single' ? (
-            <FrameDataImage
-              frameData={frameData}
-              contain={true}
-              smoothing={false}
-            />
-          ) : (
-            // TODO draw the first sub-frame by default instead of a composition of the frameData
-            // ... either that ^ or workspace cursor should select empty sub-frame by default!!!
-            <FrameDataCompositeImage
-              frameData={frameData}
-              contain={true}
-              smoothing={false}
-            />
-          )}
+          {image}
         </div>
         <div className={`${textCls} font-bold font-mono text-xs text-center px-1 py-0.5 truncate`}>
           {frameData.kind === 'multi' ? (
