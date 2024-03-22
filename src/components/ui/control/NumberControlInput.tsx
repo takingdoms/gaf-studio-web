ecfb9532b25c0956dc2598cdc0b3e53c58ee1d9c
C L <c1000@protonmail.com>
2024-03-22 19:37:01 -0300

    undid the inner value stuff for now

--- src/components/ui/control/NumberControlInput.tsx
+++ src/components/ui/control/NumberControlInput.tsx
@@ -14,67 +14,38 @@ export default function NumberControlInput({
   min,
   max,
 }: NumberControlInputProps) {
-  const [innerValue, setInnerValue] = React.useState<string>(value?.toString() ?? '');
-
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const lastNumRef = React.useRef<number>();
-
-  React.useEffect(() => {
-    const input = inputRef.current;
-
-    if (input === null) {
-      return;
-    }
-
-    const onBlur = () => {
-      if (input.value === '') {
-        input.value = lastNumRef.current?.toString() ?? '';
-      }
-    };
-
-    input.addEventListener('blur', onBlur);
-
-    return () => input.removeEventListener('blur', onBlur);
-  }, []);
-
   const onChange = React.useCallback((ev: React.ChangeEvent<HTMLInputElement>) => {
     ev.preventDefault();
-
-    const value = ev.target.value;
-    setInnerValue(value);
 
     if (setValue === undefined) {
       return;
     }
 
-    let numValue = parseInt(value);
+    let newValue = parseInt(ev.target.value);
 
-    if (Number.isNaN(numValue)) {
+    if (Number.isNaN(newValue)) {
       return;
     }
 
     if (min !== undefined) {
-      numValue = Math.max(numValue, min);
+      newValue = Math.max(newValue, min);
     }
 
     if (max !== undefined) {
-      numValue = Math.min(numValue, max);
+      newValue = Math.min(newValue, max);
     }
 
-    lastNumRef.current = numValue;
-    setValue(numValue);
-    setInnerValue(numValue.toString());
+    setValue(newValue);
   }, [min, max, setValue]);
 
   const borderCls = setValue ? 'border-gray-300' : 'border-transparent';
 
   return (
     <input
-      ref={inputRef}
       type="number"
       className={`hide-spin-button w-auto font-mono border ${borderCls} px-0.5 py-0`}
       style={{ width: CONTROL_INPUT_WIDTH, height: CONTROL_INPUT_HEIGHT }}
-      value={innerValue}
+      value={value ?? ''}
       onChange={onChange}
     />
   );
