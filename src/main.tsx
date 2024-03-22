b6396a959267419502798833c2a08238276c8918
C L <c1000@protonmail.com>
2024-03-21 21:50:23 -0300

    restored react's strict mode (removed for profiling long ago but I forgot to re-add it lol)

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import './index.css';
 import { Buffer } from 'buffer';
+import React from 'react';
 globalThis.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>,
+  </React.StrictMode>,
 );
